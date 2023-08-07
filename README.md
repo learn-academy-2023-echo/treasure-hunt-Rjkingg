30a59a1f2a8ae20131d18dd3051117960298fe2b
raquel king <kingraq1007@gmail.com>
2023-08-07 13:18:42 -0500

    Completed user story

--- README.md
+++ README.md
@@ -17,8 +17,10 @@ As a developer, you are tasked with creating a treasure hunt game. The user will
 
 
 - As a user, when I click on one of the question marks an alert appears with the index position of that question mark in the array.
+-branch tree-location
 - As a user, when I click on one of the question marks instead of the alert the question mark turns into a tree emoji.
 - As a user, if I select the winning square the question mark will become a treasure emoji and if I select the losing square the question mark will become a bomb emoji.
+treasure-bomb
 - As a user, I can click on a “Play Again” button that will restart the game.
 - As a user, I can see a counter that shows how many guesses I have left. The counter starts at five and decrements one every time I click on a square that is not the treasure nor the bomb.
 - As a user, I can see a message informing me that I won the game if I select the square that contains the treasure.
--- src/App.js
+++ src/App.js
@@ -4,23 +4,40 @@ import "./App.css"
 
 const App = () => {
   const [board, setBoard] = useState([
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?"
-  ])
+"?",
+"?",
+"?",
+"?",
+"?",
+"?",
+"?",
+"?",
+"?"
+])
+const [treasureLocation, setTreasureLocation] = useState(
+  Math.floor(Math.random() * board.length)
+)
+  const handleGamePlay = (clickedSquare) => {
+    let updateBoard = [...board]
+    //set condition for if treasure location is same as clicked index
+    if(clickedSquare === treasureLocation) {
+      updateBoard[clickedSquare] = "🕹️"
+     setBoard(updateBoard)
+    } else {
+    
+    updateBoard[clickedSquare] = "🌳"
+    setBoard(updateBoard)
+  }
+}
+
+  
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="board"> 
-      {board.map((square) => {
-    return  <Square square={square} />
+      {board.map((square, index) => {
+    return  <Square square={square} index={index} key={index} handleGamePlay={handleGamePlay}/>
       })}
     </div>
     </>
